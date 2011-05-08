fd451c01f6f103b2503178163d11cd7d3b7ec711
kaw3939 <kwilliam@njit.edu>
2011-05-08 18:37:16 +0000

    test

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+//interface for web page
+/*
+interface webpage {
+	function setHeader($pageType);
+	function setJquery($jquery);
+	function setCSS($css);
+	function getHeader();
+	function getJquery();
+	function getCSS();
+}
+*/
+//Interface for article
+interface webpage 
+{
+	public function getTitle();
+	public function setTitle($title);
+	public function getBody();
+	public function setBody($body);
+}
+class node implements webpage
+{
+	private $title;
+	private $body;
+	
+	// sets the title of the article
+	public function setTitle($title) {
+        	$this->title = $title;
+	}
+
+	// gets the title of the article 
+	public function getTitle() {
+    		return $this->title;
+ 	}
+	// gets the body of the article	
+	public function getBody() {
+		return $this->body;
+	}
+	public function setBody($body) {
+		$this->body = $body;
+	}
+} 
+$a = new node();
+$a->setTitle('keith');
+$a->setBody('body');
+
+echo '<h1>' . $a->getTitle() . '</h1>';
+echo '<p>' . $a->getBody() . '</p>';
+
+
+?>
